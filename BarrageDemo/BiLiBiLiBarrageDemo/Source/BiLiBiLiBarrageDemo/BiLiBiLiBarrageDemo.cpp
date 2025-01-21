c3fb220975b20f5e67534157310c7a1b95d354d1
Hong <guhong.usa@gmail.com>
2025-01-22 01:58:13 +0800

    添加弹幕插件 ，初始版本

--- BarrageDemo/BiLiBiLiBarrageDemo/Source/BiLiBiLiBarrageDemo/BiLiBiLiBarrageDemo.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 2025 (c) WanWanHa, LLC. All Rights Reserved.
-
-#include "BiLiBiLiBarrageDemo.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BiLiBiLiBarrageDemo, "BiLiBiLiBarrageDemo" );
- 
