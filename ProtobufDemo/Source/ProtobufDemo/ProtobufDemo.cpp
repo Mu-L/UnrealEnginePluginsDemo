f509dec360b35f31e24811e4761876636ff9707e
Hong <guhong.usa@gmail.com>
2025-04-28 01:31:23 +0800

    add protobuf demo

---
+++ ProtobufDemo/Source/ProtobufDemo/ProtobufDemo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProtobufDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProtobufDemo, "ProtobufDemo" );
